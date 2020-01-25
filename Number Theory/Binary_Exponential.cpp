c84344a532f6fb45f0682a46a62e1aa61a713bbf
Sayemm <45368565+Sayemm@users.noreply.github.com>
2020-01-25 14:55:53 +0600

    Update Binary_Exponential.cpp

--- Number Theory/Binary_Exponential.cpp
+++ Number Theory/Binary_Exponential.cpp
@@ -9,7 +9,7 @@ ll b, p, MOD;
 inline void normal(ll &a) { a%=MOD; (a<0) && (a+=MOD); }
 inline ll modMul(ll a, ll b) { a%=MOD, b%=MOD; normal(a), normal(b); return(a*b)%MOD; }
 
-ll binary_expo(ll a, ll p)
+ll binary_expo(ll a, ll p) // O(logp)
 {
     ll res = 1;
 
