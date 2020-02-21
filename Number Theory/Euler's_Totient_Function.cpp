ed5a73e97a92db61c6cac4a4919f043f54b523f2
Sayemm <45368565+Sayemm@users.noreply.github.com>
2020-02-21 15:45:32 +0600

    Add files via upload

---
+++ Number Theory/Euler's_Totient_Function.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef            long long int          ll;
+
+ll func(ll n)
+{
+    ll limit = sqrt(n+1), ans = 1;
+
+    for(int i = 2; i <= limit; i++)
+    {
+        if((n%i)==0)
+        {
+            ans *= (i-1);
+            while((n%i)==0)
+            {
+                ans *= i;
+                n /= i;
+            }
+            ans /= i;
+        }
+    }
+
+    if(n!=1) ans *= (n-1ll);
+
+    return ans;
+}
+
+int main()
+{
+    ll n;
+
+    cin >> n;
+
+    cout << func(n) << endl;
+
+    return 0;
+}
