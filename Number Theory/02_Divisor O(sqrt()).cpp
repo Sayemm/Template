f55dfed16bc21e3154881c42d774e8aaa9214832
Sayem <sayemseer@gmail.com>
2022-11-05 11:50:40 +0600

    Added divisor for less time complexity.

---
+++ Number Theory/02_Divisor O(sqrt()).cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+	int n;
+	cin >> n;
+	vector <int> div;
+
+	for (int i = 1; i * i <= n; i++) {
+		if (n % i == 0) {
+			div.push_back(i);
+			if (i != n / i) div.push_back(n / i);
+		}
+	}
+
+	sort(div.begin(), div.end());
+	for (auto x : div) {
+		cout << x << " ";
+	}
+
+	return 0;
+}
--- Number Theory/README.md
+++ Number Theory/README.md
@@ -2,3 +2,4 @@
 
 ## Divisor
 01. Time Complexity = O(n) <br/>
+02. Time Complexity = O(sqrt(n)) <br/>
